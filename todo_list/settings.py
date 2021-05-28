d9ca9e5b134461c8517f10efe1033cfb75feccf2
AtharvaCM <necrons999@gmail.com>
2021-05-28 23:30:11 +0530

    zz

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-# import django_heroku
+import django_heroku
 
 import dj_database_url
 from pathlib import Path
@@ -147,4 +147,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
