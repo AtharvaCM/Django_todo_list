e91fc0b1812c8dd03b650add0b31655689b4aa2d
AtharvaCM <necrons999@gmail.com>
2021-05-29 14:43:20 +0530

    setting updates

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -15,6 +15,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+import django_filter
+import django_storages
 
 # import environ
 # env = environ.Env(
