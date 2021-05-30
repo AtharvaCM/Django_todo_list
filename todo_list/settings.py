3aa804375aef946306a1034fc719e7bba1592487
AtharvaCM <necrons999@gmail.com>
2021-05-30 10:51:26 +0530

    dbname changes

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgresql-symmetrical-41304'
+        'NAME': 'd3g52f2spt4h38'
     }
 }
 
